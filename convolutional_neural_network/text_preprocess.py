0c69a19608fe4943c2942058386aa345fcf39ec7
Yuanjun Ren <renyj@buaa.edu.cn>
2025-02-19 22:24:20 +0800

    Add language model modules

--- convolutional_neural_network/text_preprocess.py
+++ convolutional_neural_network/text_preprocess.py
@@ -1,112 +1,16 @@
 #! coding: utf-8
-
 import unittest
-import collections
-import random
-import re
 import torch
+import matplotlib.pyplot as plt
+from pprint import pprint
 import sys
 from pathlib import Path
-
 sys.path.append(str(Path(__file__).resolve().parent.parent))
-import utils.dlf as dlf
-
-
-# Typical preprocessing pipelines execute the following steps:
-#  1. Load text as strings into memory
-#  2. Split the strings into tokens (e.g., words or characters)
-#  3. Build a vocabulary dictionary to associate each vocabulary element with a numerical index.
-#  4. Convert the text into sequences of numerical indices.
-
-
-class TimeMachine:
-    def __init__(self):
-        dlf.DATA_HUB['time_machine'] = (
-            dlf.DATA_URL + 'timemachine.txt',
-            '090b5e7e70c295757f55df93cb0a180b9691891a')
-
-    def download(self):
-        with open(dlf.download('time_machine'), 'r') as f:
-            contents = f.readlines()
-        return [re.sub('[^A-Za-z]+', ' ', line).strip().lower() for line in contents]
-
-
-class Tokenizer:
-    def __init__(self, token: str = 'word') -> None:
-        expected_tokens = ['word', 'char']
-        assert token in expected_tokens
-        self._token = token
-
-    def tokenize(self, lines):
-        if self._token == 'word':
-            return [line.split() for line in lines]
-        elif self._token == 'char':
-            return [list(line) for line in lines]
-        else:
-            print("Error: unexpected token: %s" % self._token)
-            raise KeyError
-
-
-# Construct a vocabulary for our dataset, converting the sequence of strings into
-# a list of numerical indices. Note that we have not lost any information and can
-# easily convert our dataset back to its original (string) representation.
-class Vocab:
-    def __init__(self, tokens=None, min_freq=0, reserved_tokens=None):
-        if tokens is None:
-            tokens = []
-        if reserved_tokens is None:
-            reserved_tokens = []
-
-        # Flatten a 2D list if needed
-        if tokens and isinstance(tokens[0], list):
-            tokens = [token for line in tokens for token in line]
-        # Count token frequencies
-        counter = collections.Counter(tokens)
-        self.token_freqs = sorted(counter.items(), key=lambda x: x[1], reverse=True)
-
-        # The list of unique tokens
-        self.idx_to_token = ['<unk>'] + reserved_tokens
-        self.token_to_idx = {token: idx for idx, token in enumerate(self.idx_to_token)}
-
-        for token, freq in self.token_freqs:
-            if freq < min_freq:
-                # Use break since tokens are ordered. If one is below threshold, the rest will be too low.
-                break
-
-            if token not in self.token_to_idx:
-                self.idx_to_token.append(token)
-                # We add the token to the dictionary and update the index sequentially, so the
-                # current token index is always `len(self.idx_to_token) - 1`.
-                self.token_to_idx[token] = len(self.idx_to_token) - 1
-
-    def __len__(self):
-        return len(self.idx_to_token)
-
-    def __getitem__(self, tokens):
-        if not isinstance(tokens, (list, tuple)):
-            # If `tokens` is not a valid key, return the index for the unknown token.
-            # The dict().get() method can be used to specify a default value (the second parameters).
-            return self.token_to_idx.get(tokens, self.unk)
-        return [self.__getitem__(token) for token in tokens]
-
-    def to_tokens(self, indices):
-        if not isinstance(indices, (list, tuple)):
-            return self.idx_to_token[indices]
-        return [self.idx_to_token[index] for index in indices]
-
-    @property
-    def unk(self):  # Index for the unknown token
-        return self.token_to_idx['<unk>']
-
-
-def load_corpus_time_machine(max_tokens=-1):
-    lines = TimeMachine().download()
-    tokens = Tokenizer(token='char').tokenize(lines)
-    vocab = Vocab(tokens)
-    corpus = [vocab[token] for line in tokens for token in line]
-    if max_tokens > 0:
-        corpus = corpus[:max_tokens]
-    return corpus, vocab
+from utils.plot import plot
+from recurrent_neural_network.rnn_utils import (TimeMachine, Tokenizer, Vocab,
+                                                load_corpus_time_machine,
+                                                seq_data_iter_random,
+                                                seq_data_iter_sequential)
 
 
 class IntegrationTest(unittest.TestCase):
@@ -133,6 +37,7 @@ class IntegrationTest(unittest.TestCase):
 
         # Print the ten most frequent words in the vocabulary
         print(list(vocab.token_to_idx.items())[:10])
+        pprint(vocab.token_freqs[:10])
 
         for i in [0, 10]:
             print('indices: ', tokens[i])
@@ -146,6 +51,139 @@ class IntegrationTest(unittest.TestCase):
 
         self.assertEqual(170580, len(corpus))
         self.assertEqual(28, len(vocab))
+
+    def test_random_sequence(self):
+        my_seq = list(range(35))
+        for x, y in seq_data_iter_random(my_seq, batch_size=2, num_steps=5):
+            print('X: ', x, '\nY:', y)
+            t_x, t_y = torch.tensor(x), torch.tensor(y)
+            self.assertTrue(torch.equal(t_x + 1, t_y))
+
+    def test_sequential_sequence(self):
+        my_seq = list(range(35))
+        x_prev, y_prev = None, None
+        batch_size, num_steps = 2, 5
+        for x, y in seq_data_iter_sequential(my_seq, batch_size=batch_size, num_steps=num_steps):
+            print('X: ', x, '\nY:', y)
+            t_x, t_y = torch.tensor(x), torch.tensor(y)
+            self.assertTrue(torch.equal(t_x + 1, t_y))
+
+            if x_prev is not None:
+                self.assertTrue(torch.equal(x_prev + num_steps, t_x))
+
+            x_prev, y_prev = t_x, y_prev
+
+
+class MarkovNGramModel(unittest.TestCase):
+    # Such words that are common but not particularly descriptive are often
+    # called stop words and, in previous generations of text classifiers based
+    # on so-called bag-of-words representations, they were most often filtered
+    # out. However, they carry meaning and it is not necessary to filer them
+    # out when working with modern RNN- and Transformer-based neural models.
+    #
+    # Word frequency tends to follow a power law distribution (specifically
+    # the ZipFian) as we go down the ranks.
+    # This phenomenon is captured by Zipf's law, which states that the frequency
+    # n_i of the i_th most frequent word is:
+    #     n_i === 1 / i^alpha
+    # which is equivalent to
+    #     log(n_i) = -alpha * log(i) + c
+    # where alpha is the exponent that characterizes the distribution and c is
+    # a constant. This should already give us pause for thought if we want to
+    # model words by counting statistics. After all, we will significantly
+    # overestimate the frequency of the tail, also known as the infrequent
+    # words. But what about the other word combinations, such as two consecutive
+    # words (bigrams), three consecutive words (trigrams), and beyond.
+
+    def setUp(self):
+        self.tokens = Tokenizer().tokenize(TimeMachine().download())
+
+        self.corpus = [token for line in self.tokens for token in line]
+        self.vocab = Vocab(self.corpus)
+
+    def test_unigram(self):
+        expected_output = [
+            ('the', 2261), ('i', 1267), ('and', 1245), ('of', 1155), ('a', 816),
+            ('to', 695), ('was', 552), ('in', 541), ('that', 443), ('my', 440)
+        ]
+        self.assertEqual(expected_output, self.vocab.token_freqs[:10])
+
+        freqs = [freq for token, freq in self.vocab.token_freqs]
+        plot(freqs, xlabel='token: x', ylabel='frequency: n(x)', xscale='log', yscale='log')
+
+        plt.show()
+
+    def test_bigram(self):
+        bigram_tokens = [pair for pair in zip(self.corpus[:-1], self.corpus[1:])]
+        bigram_vocab = Vocab(bigram_tokens)
+
+        expected_output = [
+            (('of', 'the'), 309),
+            (('in', 'the'), 169),
+            (('i', 'had'), 130),
+            (('i', 'was'), 112),
+            (('and', 'the'), 109),
+            (('the', 'time'), 102),
+            (('it', 'was'), 99),
+            (('to', 'the'), 85),
+            (('as', 'i'), 78),
+            (('of', 'a'), 73)]
+
+        pprint(bigram_vocab.token_freqs[:10])
+        self.assertEqual(expected_output, bigram_vocab.token_freqs[:10])
+
+    def test_trigram(self):
+        trigram_tokens = [triple for triple in zip(
+            self.corpus[:-2], self.corpus[1:-1], self.corpus[2:])]
+        trigram_vocab = Vocab(trigram_tokens)
+
+        exptected_output = [
+            (('the', 'time', 'traveller'), 59),
+            (('the', 'time', 'machine'), 30),
+            (('the', 'medical', 'man'), 24),
+            (('it', 'seemed', 'to'), 16),
+            (('it', 'was', 'a'), 15),
+            (('here', 'and', 'there'), 15),
+            (('seemed', 'to', 'me'), 14),
+            (('i', 'did', 'not'), 14),
+            (('i', 'saw', 'the'), 13),
+            (('i', 'began', 'to'), 13)]
+
+        print(trigram_vocab.token_freqs[:10])
+
+    def test_markov_ngram_model(self):
+
+        # bigram model
+        bigram_tokens = [pair for pair in zip(self.corpus[:-1], self.corpus[1:])]
+        bigram_vocab = Vocab(bigram_tokens)
+
+        # trigram model
+        trigram_tokens = [triple for triple in zip(self.corpus[:-2], self.corpus[1:-1], self.corpus[2:])]
+        trigram_vocab = Vocab(trigram_tokens)
+
+        unigram_freqs = [freq for token, freq in self.vocab.token_freqs]
+        bigram_freqs = [freq for token, freq in bigram_vocab.token_freqs]
+        trigram_freqs = [freq for token, freq in trigram_vocab.token_freqs]
+
+        plot([unigram_freqs, bigram_freqs, trigram_freqs], xlabel='token: x',
+             ylabel='frequency: n(x)', xscale='log', yscale='log',
+             legend=['unigram', 'bigram', 'trigram'])
+        # This figure is quite exciting.
+        #   1. First, beyond unigram words, sequences of words also appear to
+        #      be following Zipf's law, albeit with a smaller exponent alpha,
+        #      depending on the sequence length.
+        #   2. Second, the number of distinct n-grams is not that large. This
+        #      gives us hope that there is quite a lot of structure in language.
+        #   3. Third, many n-grams occur very rarely. This makes certain methods
+        #      unsuitable for language modeling and motivates the use of deep
+        #      learning models.
+
+        # To preprocess text, we usually:
+        #   1. split text into tokens;
+        #   2. build a vocabulary to map token strings to numerical indices;
+        #   3. convert text data into token indices for models to manipulate.
+        # In practice, the frequency of words tends to follow Zipf's law. This
+        # is true not just for individual words (unigrams), but also for n-grams.
 
 
 if __name__ == "__main__":
---
+++ recurrent_neural_network/rnn_utils.py
@@ -0,0 +1,185 @@
+#! coding: utf-8
+
+import collections
+import random
+import re
+import torch
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+import utils.dlf as dlf
+
+
+# Typical preprocessing pipelines execute the following steps:
+#  1. Load text as strings into memory
+#  2. Split the strings into tokens (e.g., words or characters)
+#  3. Build a vocabulary dictionary to associate each vocabulary element with a numerical index.
+#  4. Convert the text into sequences of numerical indices.
+
+
+class TimeMachine:
+    def __init__(self):
+        dlf.DATA_HUB['time_machine'] = (
+            dlf.DATA_URL + 'timemachine.txt',
+            '090b5e7e70c295757f55df93cb0a180b9691891a')
+
+    def download(self):
+        with open(dlf.download('time_machine'), 'r') as f:
+            contents = f.readlines()
+        return [re.sub('[^A-Za-z]+', ' ', line).strip().lower() for line in contents]
+
+
+class Tokenizer:
+    def __init__(self, token: str = 'word') -> None:
+        expected_tokens = ['word', 'char']
+        assert token in expected_tokens
+        self._token = token
+
+    def tokenize(self, lines):
+        if self._token == 'word':
+            return [line.split() for line in lines]
+        elif self._token == 'char':
+            return [list(line) for line in lines]
+        else:
+            print("Error: unexpected token: %s" % self._token)
+            raise KeyError
+
+
+# Construct a vocabulary for our dataset, converting the sequence of strings into
+# a list of numerical indices. Note that we have not lost any information and can
+# easily convert our dataset back to its original (string) representation.
+class Vocab:
+    def __init__(self, tokens=None, min_freq=0, reserved_tokens=None):
+        if tokens is None:
+            tokens = []
+        if reserved_tokens is None:
+            reserved_tokens = []
+
+        # Flatten a 2D list if needed
+        if tokens and isinstance(tokens[0], list):
+            tokens = [token for line in tokens for token in line]
+        # Count token frequencies
+        counter = collections.Counter(tokens)
+        self.token_freqs = sorted(counter.items(), key=lambda x: x[1], reverse=True)
+
+        # The list of unique tokens
+        self.idx_to_token = ['<unk>'] + reserved_tokens
+        self.token_to_idx = {token: idx for idx, token in enumerate(self.idx_to_token)}
+
+        for token, freq in self.token_freqs:
+            if freq < min_freq:
+                # Use break since tokens are ordered. If one is below threshold, the rest will be too low.
+                break
+
+            if token not in self.token_to_idx:
+                self.idx_to_token.append(token)
+                # We add the token to the dictionary and update the index sequentially, so the
+                # current token index is always `len(self.idx_to_token) - 1`.
+                self.token_to_idx[token] = len(self.idx_to_token) - 1
+
+    def __len__(self):
+        return len(self.idx_to_token)
+
+    def __getitem__(self, tokens):
+        if not isinstance(tokens, (list, tuple)):
+            # If `tokens` is not a valid key, return the index for the unknown token.
+            # The dict().get() method can be used to specify a default value (the second parameters).
+            return self.token_to_idx.get(tokens, self.unk)
+        return [self.__getitem__(token) for token in tokens]
+
+    def to_tokens(self, indices):
+        if not isinstance(indices, (list, tuple)):
+            return self.idx_to_token[indices]
+        return [self.idx_to_token[index] for index in indices]
+
+    @property
+    def unk(self):  # Index for the unknown token
+        return self.token_to_idx['<unk>']
+
+
+def load_corpus_time_machine(max_tokens=-1):
+    lines = TimeMachine().download()
+    tokens = Tokenizer(token='char').tokenize(lines)
+    vocab = Vocab(tokens)
+    corpus = [vocab[token] for line in tokens for token in line]
+    if max_tokens > 0:
+        corpus = corpus[:max_tokens]
+    return corpus, vocab
+
+
+def seq_data_iter_random(corpus, batch_size, num_steps):
+    """
+    Use random sampling method to generate a batch of sequences
+    :param corpus:
+    :param batch_size:
+    :param num_steps:
+    :return:
+    """
+    # Crop a random starting sequence, the random sequence length is [0, num_steps - 1]
+    corpus = corpus[random.randint(0, num_steps - 1):]
+    # We should consider the label, so we minus 1
+    num_subseqs = (len(corpus) - 1) // num_steps
+
+    # Calculate the starting indices for each subsequence
+    initial_indices = list(range(0, num_subseqs * num_steps, num_steps))
+    # In random sampling, the subsequences from two adjacent random minibatches are
+    # not necessarily adjacent on the original sequence.
+    random.shuffle(initial_indices)
+
+    def data(pos):
+        # Return a sequence of length `num_steps` starting from `pos`
+        return corpus[pos: pos + num_steps]
+
+    num_batches = num_subseqs // batch_size
+    for i in range(0, batch_size * num_batches, batch_size):
+        # Here, `initial_indices` contains randomized starting indices for subsequences
+        initial_indices_per_batch = initial_indices[i: i + batch_size]
+        x = [data(j) for j in initial_indices_per_batch]
+        y = [data(j + 1) for j in initial_indices_per_batch]
+        yield torch.tensor(x), torch.tensor(y)
+
+
+def seq_data_iter_sequential(corpus, batch_size, num_steps):
+    """
+    Use sequential sampling method to generate a batch of sequences
+    :param corpus:
+    :param batch_size:
+    :param num_steps:
+    :return:
+    """
+    # Start from a random offset to avoid starting at the same position
+    offset = random.randint(0, num_steps)
+    num_tokens = ((len(corpus) - offset - 1) // batch_size) * batch_size
+    x = torch.tensor(corpus[offset: offset + num_tokens])
+    y = torch.tensor(corpus[offset + 1: offset + 1 + num_tokens])
+
+    x = x.reshape(batch_size, -1)
+    y = y.reshape(batch_size, -1)
+
+    num_batches = x.shape[1] // num_steps
+    for i in range(0, num_batches * num_steps, num_steps):
+        x_batch = x[:, i: i + num_steps]
+        y_batch = y[:, i: i + num_steps]
+        yield x_batch, y_batch
+
+
+class SeqDataLoader:
+    """
+    Load sequence data in mini-batches
+    """
+    def __init__(self, batch_size, num_steps, use_random_iter, max_tokens):
+        if use_random_iter:
+            self.data_iter_fn = seq_data_iter_random
+        else:
+            self.data_iter_fn = seq_data_iter_sequential
+        self.corpus, self.vocab = load_corpus_time_machine(max_tokens)
+        self.batch_size, self.num_steps = batch_size, num_steps
+
+    def __iter__(self):
+        return self.data_iter_fn(self.corpus, self.batch_size, self.num_steps)
+
+
+def load_data_time_machine(batch_size, num_steps, use_random_iter=False, max_tokens=10000):
+    """ Load the time machine dataset """
+    data_iter = SeqDataLoader(batch_size, num_steps, use_random_iter, max_tokens)
+    return data_iter, data_iter.vocab
